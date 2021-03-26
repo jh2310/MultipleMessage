0da654c64d2029168482b631f34df79d490537b1
jh <2310237964@qq.com>
2021-03-26 10:53:06 +0800

    第一次测试提交

---
+++ src/main/java/com/jh/multipleMessage/App.java
@@ -0,0 +1,21 @@
+package com.jh.multipleMessage;
+
+import org.junit.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    @Test
+    public void test() {
+    	System.out.println("test success");
+    }
+    
+}
